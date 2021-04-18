f28bb5df99f90f6189c2c02c02b1952360b0b3ea
Michael Maxwell <mike.ian.maxwell@gmail.com>
2021-04-17 18:35:37 -0700

    MVP status

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const playing = require('spotify-playing')
+
+function getGeniusLink(song) {
+    // Given a song object {artist, song} return the genius lyrics webpage
+    let artist = song.artist.replace(/\s+/g, '-').toLowerCase()
+    let title = song.song.replace(/\s+/g, '-').toLowerCase()
+    return `https://genius.com/${artist}-${title}-lyrics`
+}
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1050,
+    height: 1300,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
-  win.loadFile('index.html')
+  let last = {}
+  setInterval(playing, 2000, (err, now) => {
+        if (err)
+            return console.error(err)
+    
+        if (JSON.stringify(now) !== JSON.stringify(last)) {
+            let geniusLink = getGeniusLink(now)
+            console.log(now, geniusLink)
+            win.loadURL(geniusLink);
+
+            // Set last to now
+            last = now
+        }
+    })
 }
 
 app.whenReady().then(() => {
